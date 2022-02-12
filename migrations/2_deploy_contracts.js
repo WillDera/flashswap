26dad91aff341f70f7eecd3dce4e3b16d4323a21
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-13 00:47:07 +0100

    transfer all tokens to FlashSwap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,15 @@
 const FlashSwap = artifacts.require("FlashSwap");
 const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   // deploy flashswap
-  deployer.deploy(FlashSwap);
+  await deployer.deploy(FlashSwap);
+  const flashSwap = await FlashSwap.deployed();
 
   // deploy token
-  deployer.deploy(Token);
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  // transfer all tokens to FlashSwap (DEX)
+  await token.transfer(flashSwap.address, "1000000000000000000000000");
 };
