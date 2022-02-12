b336a6e4e1c7d3eaf06fd208b7907f1b3c800a67
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-13 00:30:18 +0100

    Token
    - Create token
    - Run migrations to deploy token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,11 @@
-// eslint-disable-next-line no-undef
-const FlashSwap = artifacts.require("Migrations");
+/* eslint-disable no-undef */
+const FlashSwap = artifacts.require("FlashSwap");
+const Token = artifacts.require("Token");
 
 module.exports = function(deployer) {
+  // deploy flashswap
   deployer.deploy(FlashSwap);
+
+  // deploy token
+  deployer.deploy(Token);
 };
