e900086c930984d2f9926aa5d1c4d31e50d54a72
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-13 21:41:45 +0100

    chai assertion
    - Assertions to check for:
    a) Token deployment
    b) token transfer
    c) token balance

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.5.0;
 contract Token {
     string public name = "DApp Token";
     string public symbol = "DAPP";
-    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens  (1m + 18 zeros)
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens  (expressed in Wei)
     uint8 public decimals = 18;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
--- test/FlashSwap.test.js
+++ test/FlashSwap.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 const { assert } = require("chai");
+const { default: Web3 } = require("web3");
 
 // import contracts
 const FlashSwap = artifacts.require("FlashSwap");
@@ -11,17 +12,39 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
+// convert value passed to wei e.g 1000000 => 1000000000000000000000000
+function tokenHelper(n) {
+  return web3.utils.toWei(n, "ether");
+}
+
 contract("FlashSwap", (accounts) => {
+  let flashSwap, token;
+
+  before(async () => {
+    flashSwap = await FlashSwap.new();
+    token = await Token.new();
+    // transfer all tokens to FlashSwap (DEX)
+    await token.transfer(flashSwap.address, tokenHelper("1000000"));
+  });
+
   describe("FlashSwap deployment", async () => {
     it("contract has a name", async () => {
-      // arrange
-      let flashSwap = await FlashSwap.new();
-
-      // act
       const name = await flashSwap.name();
-
-      // assert
       assert.equal(name, "FlashSwap");
+    });
+
+    it("contract has tokens", async () => {
+      let balance = await token.balanceOf(flashSwap.address);
+
+      assert.equal(balance.toString(), tokenHelper("1000000"));
+    });
+  });
+
+  describe("Token deployment", async () => {
+    it("contract has a name", async () => {
+      const name = await token.name();
+
+      assert.equal(name, "DApp Token");
     });
   });
 });
