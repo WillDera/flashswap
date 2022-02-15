f3f9529575e5396d8bf87ba4cbf0b08987d0cf6e
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-15 23:30:18 +0100

    Buy and sell
    - loaded token and dex contracts
    - Added buy and sell pages
    - Added logo for eth and dapp token

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
 import React, { Component } from "react";
 import Web3 from "web3";
+import Token from "../abis/Token.json";
+import FlashSwap from "../abis/FlashSwap.json";
 import NavBar from "./Navbar";
+import Main from "./Main";
 import "./App.css";
 
 class App extends Component {
@@ -18,6 +21,35 @@ class App extends Component {
 
     const ethBalance = await web3.eth.getBalance(this.state.account);
     this.setState({ ethBalance });
+
+    const networkId = await web3.eth.net.getId();
+
+    // Load Token
+    const tokenData = Token.networks[networkId];
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address);
+      this.setState({ token });
+      let tokenBalance = await token.methods
+        .balanceOf(this.state.account)
+        .call();
+      this.setState({ tokenBalance: tokenBalance.toString() });
+    } else {
+      window.alert("Token contract not deployed to detected network.");
+    }
+
+    // Load FlashSwap
+    const flashSwapData = FlashSwap.networks[networkId];
+    if (flashSwapData) {
+      const flashSwap = new web3.eth.Contract(
+        FlashSwap.abi,
+        flashSwapData.address
+      );
+      this.setState({ flashSwap });
+    } else {
+      window.alert("FlashSwap contract not deployed to detected network.");
+    }
+
+    this.setState({ loading: false });
   }
 
   async loadWeb3() {
@@ -55,20 +87,42 @@ class App extends Component {
     super(props);
     this.state = {
       account: "",
+      token: {},
+      flashSwap: {},
       ethBalance: "0",
+      tokenBalance: "0",
+      loading: true,
     };
   }
 
   render() {
+    let content;
+    if (this.state.loading) {
+      content = (
+        <p id="loader" className="text-center">
+          Loading...
+        </p>
+      );
+    } else {
+      content = (
+        <Main
+          ethBalance={this.state.ethBalance}
+          tokenBalance={this.state.tokenBalance}
+        />
+      );
+    }
+
     return (
       <div>
         <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <h1>Vamos</h1>
-              </div>
+            <main
+              role="main"
+              className="col-lg-12 ml-auto mr-auto"
+              style={{ maxWidth: "600px" }}
+            >
+              <div className="content mr-auto ml-auto">{content}</div>
             </main>
           </div>
         </div>
