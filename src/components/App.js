b9e7b7b94749ae8b77ab82039c926b3481a5f6a0
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-15 21:57:06 +0100

    Frontend
    - Setup frontend to connect to metamask via web3.
    
    - Autogenerate wallet icon via Identicon library.

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,73 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+/* eslint-disable no-undef */
+import React, { Component } from "react";
+import Web3 from "web3";
+import NavBar from "./Navbar";
+import "./App.css";
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+  }
+
+  async loadWeb3() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        web3.eth.sendTransaction({
+          /* ... */
+        });
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      web3.eth.sendTransaction({
+        /* ... */
+      });
+    }
+    // Non-dapp browsers...
+    else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      ethBalance: "0",
+    };
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Vamos</h1>
               </div>
             </main>
           </div>
